072ebc9d736ee1908e0068cde32b09c2c731ca26
nomyeongjin <bbagifa2555@gmail.com>
2024-04-25 17:51:02 +0900

    입양 페이지 (미완)

--- pawLife/src/main/resources/static/js/adoption/adoptionWrite.js
+++ pawLife/src/main/resources/static/js/adoption/adoptionWrite.js
@@ -3,23 +3,33 @@ submitPost = function() {
   
    const title = document.querySelector("#adoptTitle").value;
    const content = document.querySelector("#adoptContent").value;
+   const infoName = document.querySelector("#infoName").value;
+   const infoAge = document.querySelector("#infoAge").value;
+   const infoType = document.querySelector("#infoType").value;
+   const infoAddress = document.querySelector("#infoAddress").value;
+ 
+
 
    console.log(title);
    console.log(content);
 
    const obj = {
        "adoptTitle" : title,
-       "adoptContent" : content
+       "adoptContent" : content,
+       "infoName":infoName,
+       "infoAge":infoAge,
+       "infoType":infoType,
+       "infoAddress":infoAddress
    };
 
-   fetch("/adoption/adoptionWrite", {
-       method : "POST",
-       headers : {"Content-Type" : "application/json"},
-       body : JSON.stringify(obj)
-   })
-   .then( resp => resp.text() )
-   .then( result => {
-       console.log(result);
-   })
+//    fetch("/adoption/adoptionWrite", {
+//        method : "POST",
+//        headers : {"Content-Type" : "application/json"},
+//        body : JSON.stringify(obj)
+//    })
+//    .then( resp => resp.text() )
+//    .then( result => {
+//        console.log(result);
+//    })
 
 }
