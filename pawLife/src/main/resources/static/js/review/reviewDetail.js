731e56fd854c00643cb23ab2464c935a19b6e958
user1 <cicadax31@gmail.com>
2024-04-30 17:25:32 +0900

    댓글 등록, 삭제 추가

--- pawLife/src/main/resources/static/js/review/reviewDetail.js
+++ pawLife/src/main/resources/static/js/review/reviewDetail.js
@@ -8,13 +8,10 @@
 /* 댓글 목록 조회 */
 const selectCommentList = () => {
 
-    // console.log(reviewNo);
 
     fetch("/comment?reviewNo=" + reviewNo)
     .then(resp => resp.json())
     .then(commentList => {
-
-        console.log(commentList);
 
         const ul = document.querySelector("#commentList");
         ul.innerHTML = "";
@@ -58,7 +55,7 @@ const selectCommentList = () => {
 
                 const updateBtn = document.createElement("button");
                 updateBtn.innerText = "수정";
-                updateBtn.setAttribute("onclick", `showUpdateComment(${comment.commentNo}, this)`);
+                updateBtn.setAttribute("onclick", `UpdateComment(${comment.commentNo}, this)`);
 
                 const deleteBtn = document.createElement("button");
                 deleteBtn.innerText = "삭제";
@@ -72,5 +69,78 @@ const selectCommentList = () => {
         }
     })
 }
+
+/* 댓글 등록 */
+const commentContent = document.querySelector("#commentContent");
+const addComment = document.querySelector("#addComment");
+
+addComment.addEventListener("click", e => {
+
+    if(loginMemberNo == null) {
+        alert("로그인후 이용해주세요.");
+        return;
+    }
+
+    if(commentContent.value.trim().length == 0){
+        alert(" 내용 작성후 등록 버튼을 클릭해주세요.");
+        commentContent.focus();
+        return;
+    }
+
+    const data = {
+        "commentContent" : commentContent.value,
+        "reviewNo" : reviewNo,
+        "memberNo" : loginMemberNo
+    };
+
+    fetch("/comment", {
+        method : "POST",
+        headers : {"content-Type" : "application/json"},
+        body : JSON.stringify(data)
+    })
+    .then(resp => resp.text())
+    .then(result => {
+
+        if(result > 0) {
+            alert("댓글이 등록되었습니다.");
+            commentContent.value = "";
+            selectCommentList();
+        }
+        else alert("댓글 등록 오류");
+    })
+    .catch( err => console.log(err));
+});
+
+
+/* 댓글 삭제 */
+const deleteComment = commentNo => {
+
+    console.log(commentNo);
+
+    if( !confirm("삭제 하시겠습니까?")) {
+        return;
+    }
+
+    fetch("/comment", {
+        method : "DELETE",
+        headers : {"content-Type" : "application/json"},
+        body : commentNo
+    })
+    .then(resp => resp.text())
+    .then(result => {
+
+        if(result > 0) {
+            alert("댓글이 삭제되었습니다.");
+            selectCommentList();
+        }
+        else    alert("댓글 삭제 실패");
+    })
+    .catch( err => console.log(err));
+
+};
+
+
+
+
 
 selectCommentList();
